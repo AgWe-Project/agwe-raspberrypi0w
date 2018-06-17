c360f3f1b937d56dfa95c7e9b0fbcb7e1d290117
Zach Jones <prozachj@gmail.com>
2018-06-17 18:33:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AgWe pi zero w Adapters
+
+Exposes data via websocket server for AgWe controller.
+
+Will show basic web interface at http://piipaddress whenever you are connected to the same wireless network or your pi's enabled wireless access point.
+
+![AgWe Tessel Web UI](https://raw.githubusercontent.com/AgWe-Project/AgWe/gh-pages/AgWe%20Tessel%20Web%20UI.png)
---
+++ socket-climate.js
@@ -0,0 +1,27 @@
+var ip = require('ip');
+//Import pi0w hardware interfaces
+var sensor = require('node-dht-sensor');
+var handler = require('./server.js');
+
+//Start Socket.Import
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+
+var monitor = {
+  temphumidListener: function () {
+    sensor.read(22, 4, function(err, temperature, humidity) {
+      if (!err) {
+        io.emit('temp', {temp: temperature.toFixed(1)});
+        io.emit('humid', {humid: humidity.toFixed(1)});
+      }
+    });	
+  }
+};
+
+io.on('connection', function (socket) {
+  console.log("Socket IO Connected");
+  setInterval(monitor.temphumidListener, 30000);
+});
+
+app.listen(80);
+console.log("Server running at ", ip.address());
